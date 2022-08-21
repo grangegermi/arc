31ab2fda5a312e4b2ba4e9024297abb5eb1450b9
grangegermi <dasha.voloshina@gmail.com>
2022-08-21 12:22:24 +0300

    Initial Commit

---
+++ someproject2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  someproject2
+//
+//  Created by Даша Волошина on 21.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
